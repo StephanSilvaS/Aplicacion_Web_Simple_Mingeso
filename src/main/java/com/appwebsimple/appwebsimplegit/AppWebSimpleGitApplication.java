56a42985c0fdc0f770b4d94c5dcea75ae45a4d19
StephanSilvaS <stephan.silva@usach.cl>
2022-04-18 10:20:34 -0400

    Se agrega a repo el proyecto base en SpringBoot

---
+++ src/main/java/com/appwebsimple/appwebsimplegit/AppWebSimpleGitApplication.java
@@ -0,0 +1,13 @@
+package com.appwebsimple.appwebsimplegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebSimpleGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebSimpleGitApplication.class, args);
+	}
+
+}
