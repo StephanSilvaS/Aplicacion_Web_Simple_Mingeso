e8dabdadf48c9e1fb31f44301d1508f67a683176
StephanSilvaS <stephan.silva@usach.cl>
2022-04-18 11:08:11 -0400

    Se corrige error 404 generado en archivo HelloWorld

--- src/main/java/com/appwebsimple/appwebsimplegit/controller/HelloWorld.java
+++ src/main/java/com/appwebsimple/appwebsimplegit/controller/HelloWorld.java
@@ -1,15 +1,15 @@
 package com.appwebsimple.appwebsimplegit.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/home")
+@RestController
+@RequestMapping("/")
 public class HelloWorld {
 
     @RequestMapping(method = RequestMethod.GET)
-    
+
     public String home(){
         return "Aplicación web muy simple";
     }
